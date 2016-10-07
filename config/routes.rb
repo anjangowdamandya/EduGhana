01eeff853b96e7d984fc499a6bc0277c4bf9acbc
anjankumarhn <anjankumar@artrium.me>
2016-10-07 15:15:04 +0530

    Done with Batch model

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :schools
   resources :terms_semister
   resources :grading_levels
+  resources :batches
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
