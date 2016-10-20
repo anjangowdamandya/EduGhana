49bf6bbe5138333a94a403d9fe94ad0ba5c5c7ee
Akshaysimha <akshay.simha91@gmail.com>
2016-10-20 22:43:57 +0530

    Changes as per client feedback

--- app/models/grading_level.rb
+++ app/models/grading_level.rb
@@ -1,7 +1,7 @@
 class GradingLevel < ActiveRecord::Base
 
 	#Validations
-	validates :name, presence: true, :length => { :minimum => 3 }
+	validates :name, presence: true
 	validates :minimum_marks, presence: true, :numericality => {:only_integer => true}
 	validates :maximum_marks, presence: true, :numericality => {:only_integer => true}
 	validates :grade_point, presence: true
